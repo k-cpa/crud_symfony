698bddfd4aec4a1b26a85b1f2ac71cc386458d91
Kev <kevin@MacBookPro.lan>
2025-01-22 12:22:13 +0100

    dashboard ok

--- src/Controller/Admin/PizzaCrudController.php
+++ src/Controller/Admin/PizzaCrudController.php
@@ -23,31 +23,24 @@ class PizzaCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
+
+        // Tu peux laisser tout commenté et ça affiche tout automatiquement !! 
         return [
-            IdField::new('id'),
             TextField::new('name'),
             TextField::new('content'),
 
             // Liste des ingrédients -> gestion 'Array'
-            AssociationField::new('ingredient', 'name')
-                ->formatValue(function ($value) {
-                    // Si $value est une collection, on peut afficher les noms des ingrédients
-                    if ($value) {
-                        return implode(', ', array_map(function ($ingredient) {
-                            return $ingredient->getName(); // Affiche le nom de chaque ingrédient
-                        }, $value->toArray()));
-                    }
-                    return 'Aucun ingrédient';
-                })
-                ->onlyOnIndex(), // Affiche dans l'index
+            AssociationField::new('ingredient', 'ingredients')
+                ->setFormTypeOptions([
+                    'by_reference' => false,
+                    'multiple' => true, 
+                    'choice_label'=> 'name', // champ à récupérer
+                ]),
 
             // Display de l'image -> Gestion display 'FileType'
             ImageField::new('imageName', 'image')
-                ->onlyOnIndex()
-                ->setBasePath('uploads/images/'),
-            ImageField::new('imageFile', 'image')
-                ->onlyOnForms()
-                ->setFormType(FileType::class)
+                ->setBasePath('uploads/images/')
+                ->setUploadDir('public/uploads/images/')
         ];
     }
 }
